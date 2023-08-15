741a8a61b21b7373ea367ca532b67899c91d3c05
pk <prettykool@protonmail.com>
2023-08-15 14:29:43 -0500

    Update mod.ts
    hopefully this shouldn't cause any more problems in a year.

--- mod.ts
+++ mod.ts
@@ -1,2 +1,1 @@
-// @deno-types="https://cdn.jsdelivr.net/npm/@types/marked@latest/index.d.ts"
-export * from "https://cdn.jsdelivr.net/npm/marked@latest/lib/marked.esm.js";
+export * from "npm:marked";
